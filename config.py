99745bae2767fddfe16d8695f6dd793dee1cd6e2
Aaron Schlegel <aaron@aaronschlegel.com>
2018-07-05 20:22:45 -0700

    commit changes

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    SECRET_KEY = os.environ['POETFLASK_SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['POETFLASK_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
