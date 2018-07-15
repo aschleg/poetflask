fba7722b482c562f81942303b708e1b031e27359
Aaron Schlegel <aaron@aaronschlegel.com>
2018-07-14 17:55:45 -0700

    commit latest changes

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,7 @@ db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = PoetryForms()
     author = None
@@ -21,9 +22,20 @@ def index():
     return render_template('index.html', title='Poetflask', form=form, author_poems=author_poems)
 
 
-# @app.route('/author', methods=['GET', 'POST'])
-# def author():
-#     author = request.form['author_select']
-#     author_poems = Poems.query.filter_by(author=author).all()
-#
-#     return render_template('author.html', author_poems=author_poems, author=author)
+@app.route('/authors', methods=['GET', 'POST'])
+def authors_list():
+    author_list = Author.query.all()
+
+    return render_template('authors.html', author_list=author_list)
+
+
+@app.route('/authors/<author>/', methods=['GET', 'POST'])
+def author_poetry(author):
+    form = PoetryForms()
+
+    if request.method == 'POST':
+        author = form.author_select.data.author
+
+    author_poems = Poems.query.filter_by(author=author).all()
+
+    return render_template('author.html', author_poems=author_poems, author_name=author)#, author=author)
