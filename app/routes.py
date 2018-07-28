37f9b9968b97c6f06a35472049df81619de3ddf9
Aaron Schlegel <aaron@aaronschlegel.com>
2018-07-28 06:36:41 -0700

    commit recent changes

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,6 @@ from flask import render_template, flash, redirect, request
 from sqlalchemy import func
 
 from app import app
-from app.forms import PoetryForms
 from app.models import Author, Poems, db
 
 
@@ -12,29 +11,21 @@ db.create_all()
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = PoetryForms()
-    author = None
 
-    if request.method == 'POST':
-        author = form.author_select.data.author
-
-    author_poems = Poems.query.\
-        filter_by(author=author).\
-        all()
-
-    return render_template('index.html', title='Poetflask', form=form, author_poems=author_poems)
+    return render_template('index.html', title='Poetflask')
 
 
-@app.route('/authors', methods=['GET', 'POST'])
+@app.route('/authors/', methods=['GET', 'POST'])
 def authors_list():
     author_list = Author.query.all()
-    author_poem_count = Poems.query.\
-        with_entities(Poems.author, func.count(Poems.title).label('poem_count')).\
-        group_by(Poems.author).\
-        order_by(Poems.author.asc())\
-        .all()
 
-    return render_template('authors.html', author_list=author_list)
+    # author_poem_count = Poems.query.\
+    #     with_entities(Poems.author, func.count(Poems.title).label('poem_count')).\
+    #     group_by(Poems.author).\
+    #     order_by(Poems.author.asc())\
+    #     .all()
+    title = 'Poets'
+    return render_template('authors.html', author_list=author_list, title=title)
 
 
 @app.route('/authors/<author>/', methods=['GET', 'POST'])
